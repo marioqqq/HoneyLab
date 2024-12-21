68afad3066beb3f070e0ccb8a6ef804a35d5199f
Mario Michalik <michalikmario99@gmail.com>
2024-12-21 14:59:12 +0100

    Changes in personal

--- PC-Personal/postInstall.sh
+++ PC-Personal/postInstall.sh
@@ -4,6 +4,7 @@
 postInstall(){
     clear
     sudo apt update && sudo apt upgrade -y
+    sudo ubuntu-drivers install
 }
 
 # Install and setup services
@@ -15,11 +16,14 @@ installServices(){
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     sudo apt update
     sudo apt install brave-browser -y
-    sudo apt remove firefox -y
+    sudo apt install snap -y
+    sudo snap remove firefox
     sudo apt install docker.io -y
-    sudo apt install docker-compose -y
+    mkdir -p ~/.docker/cli-plugins/
+    curl -SL https://github.com/docker/compose/releases/download/v2.31.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+    chmod +x ~/.docker/cli-plugins/docker-compose
     sudo usermod -aG docker $USER
-    sudo apt install code -y
+    sudo snap install --classic code
     sudo add-apt-repository ppa:serge-rider/dbeaver-ce
     sudo apt update
     sudo apt install dbeaver-ce -y
